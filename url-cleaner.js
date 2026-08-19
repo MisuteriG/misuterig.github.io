d2422e04d67addc73e6fbe7d7e6401657d0ca7ec
MisuteriG <misuterig.bgtj@gmail.com>
2026-08-19 12:39:08 +0700

    Add files via upload

--- url-cleaner.js
+++ url-cleaner.js
@@ -1,13 +1,16 @@
 (function() {
     const path = window.location.pathname;
-    let newPath;
+    let newPath = null;
 
     if (path.endsWith('/index.html')) {
         // Xử lý riêng cho index.html, chuyển hướng về thư mục gốc
-        newPath = path.substring(0, path.length - 10); // Bỏ 'index.html'
+        newPath = path.substring(0, path.length - 10) || '/'; // Bỏ 'index.html', đảm bảo kết quả là '/' cho trang chủ
     } else if (path.endsWith('.html') && path !== '/') {
         // Xử lý cho các tệp .html khác
-        const newPath = path.substring(0, path.length - 5); // Xóa đuôi '.html'
+        newPath = path.substring(0, path.length - 5); // Xóa đuôi '.html'
+    }
+
+    if (newPath) {
         const newUrl = window.location.protocol + "//" + window.location.host + newPath + window.location.search + window.location.hash;
         // Thay thế URL trong lịch sử trình duyệt mà không tải lại trang
         window.history.replaceState({path: newUrl}, '', newUrl);
